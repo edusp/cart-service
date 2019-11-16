723523c47f0a2dd965ca627d15fe39c8c5d1e8aa
edpereira <edpereira@ebay.com>
2019-11-16 11:34:02 +1100

    Joke

--- src/main/java/com/bt/au/shoppingcart/controllers/JokeController.java
+++ src/main/java/com/bt/au/shoppingcart/controllers/JokeController.java
@@ -1,7 +1,5 @@
 package com.bt.au.shoppingcart.controllers;
 
-import com.bt.au.shoppingcart.repositories.UserRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -13,31 +11,24 @@ import org.springframework.web.bind.annotation.RestController;
 public class JokeController {
 
 
-    private UserRepository repository;
-
-    @Autowired
-    public JokeController(UserRepository repository) {
-        this.repository = repository;
-    }
-
     @GetMapping("/{name}")
     public ResponseEntity get(@PathVariable("name") String name) {
 
         name = name.toUpperCase();
 
         if ("gabriel".equalsIgnoreCase(name)) {
-            return ResponseEntity.ok(name + " Mané !");
+            return ResponseEntity.ok("<h1>"+name + " Mané ! </h1>");
         }
         else if ("raquel".equalsIgnoreCase(name)) {
-            return ResponseEntity.ok(name + " Fuinha !");
+            return ResponseEntity.ok("<h1>"+name + " Fuinha ! </h1>");
         }else if ("bianca".equalsIgnoreCase(name)) {
-            return ResponseEntity.ok(name + " PiuPiu !");
+            return ResponseEntity.ok("<h1>"+name + " PiuPiu ! </h1>");
         }else if ("edu".equalsIgnoreCase(name)) {
-            return ResponseEntity.ok(name + " Very smart and Gabriel is Bafão");
+            return ResponseEntity.ok("<h1>"+name + " Very smart and Gabriel is Bafão </h1>");
         }else if ("bruna".equalsIgnoreCase(name)) {
-            return ResponseEntity.ok(name + " Linda");
+            return ResponseEntity.ok("<h1>"+name + " Linda </h1>");
         }else {
-            return ResponseEntity.ok("Try again.");
+            return ResponseEntity.ok("<h1>Try again.</h1>");
         }
 
     }
