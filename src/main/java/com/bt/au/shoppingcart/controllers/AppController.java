96a5bcfd2a31c26d56acb28bbef610f56dfe7c00
edpereira <edpereira@ebay.com>
2020-07-19 20:22:34 +1000

    Added app controller

--- src/main/java/com/bt/au/shoppingcart/controllers/AppController.java
+++ src/main/java/com/bt/au/shoppingcart/controllers/AppController.java
@@ -3,18 +3,20 @@ package com.bt.au.shoppingcart.controllers;
 
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("app")
 public class AppController {
 
-
-
     @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<String> test() {
+    public ResponseEntity<String> appGet() {
         return ResponseEntity.ok("Working");
     }
+
+    @PostMapping("{text}")
+    public ResponseEntity<String> postText(@PathVariable(value = "text") String text) {
+        return ResponseEntity.ok(text);
+    }
+
 }
